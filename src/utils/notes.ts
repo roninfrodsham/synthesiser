c25243263779acb2a524f8e1d4f422cdacd4668f
roninfrodsham <roninfrodsham@gmail.com>
2024-02-09 10:02:39 +0000

    add vitest for more reliability

--- src/utils/notes.ts
+++ src/utils/notes.ts
@@ -1,4 +1,4 @@
-function getAllNaturalNotes(naturalNotes: Array<string>, [firstKey, lastKey]: Array<string>) {
+function getAllNaturalNotes(naturalNotes: string[], [firstKey, lastKey]: [string, string]): string[] {
   const firstNote = firstKey.slice(0, 1);
   const firstOctave = parseInt(firstKey.slice(1));
   const firstNotePosition = naturalNotes.indexOf(firstNote);
@@ -6,15 +6,18 @@ function getAllNaturalNotes(naturalNotes: Array<string>, [firstKey, lastKey]: Ar
   const lastOctave = parseInt(lastKey.slice(1));
   const lastNotePosition = naturalNotes.indexOf(lastNote);
 
-  const allNaturalNotes: Array<string> = [];
+  if (firstNotePosition === -1 || lastNotePosition === -1) {
+    throw new Error("Invalid key provided");
+  }
+
+  const allNaturalNotes: string[] = [];
 
   for (let octave = firstOctave; octave <= lastOctave; octave++) {
     const start = octave === firstOctave ? firstNotePosition : 0;
     const end = octave === lastOctave ? lastNotePosition + 1 : naturalNotes.length;
 
-    naturalNotes.slice(start, end).forEach((note) => {
-      allNaturalNotes.push(`${note}${octave}`);
-    });
+    const notes = naturalNotes.slice(start, end).map((note) => `${note}${octave}`);
+    allNaturalNotes.push(...notes);
   }
 
   return allNaturalNotes;
